8b22b6e22b494519b6dac8f40d1f8e2ddf957c44
Robin Cole <robmarkcole@gmail.com>
2021-04-12 05:42:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    '''Index page route'''
-
-    return '<h1>Application Deployed!</h1>'
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True, host='0.0.0.0')
